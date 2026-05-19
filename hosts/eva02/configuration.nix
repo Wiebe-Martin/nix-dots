3f6f68b3d23e3b890880b91694d75b6917d80b61
Martin Desktop <m.wiebe311204@gmail.com>
2026-05-19 17:26:37 +0200

    lasdjfl;asdfily

--- hosts/eva02/configuration.nix
+++ hosts/eva02/configuration.nix
@@ -5,99 +5,61 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+    imports = [
+        # Include the results of the hardware scan.
+        ./hardware-configuration.nix
+
+        ../../modules/system/systemd-boot.nix
+        ../../modules/system/locals.nix
+
+        ../../modules/audio/pipewire.nix
+
+        ../../modules/shell/zsh.nix
+
+        ../../modules/net/networkmanager.nix
+
+        ../../modules/windowmanager/hyprland.nix
+
+        ../../modules/misc/smb.nix
     ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+    networking.hostName = "eva02";
 
-  networking.hostName = "eva02"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    environment.pathsToLink = [
+        "/share/applications"
+        "/share/xdg-desktop-portal"
+    ];
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    services.xserver.xkb = {
+        layout = "eu";
+        variant = "";
+    };
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+    users.users.martin = {
+        isNormalUser = true;
+        description = "Martin Wiebe";
+        extraGroups = [
+            "networkmanager"
+            "wheel"
+        ];
+        packages = with pkgs; [ ];
+    };
 
-  # Set your time zone.
-  time.timeZone = "Europe/Berlin";
+    nixpkgs.config.allowUnfree = true;
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+    environment.systemPackages = with pkgs; [
+        wget
+        git
+        vim
+    ];
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "de_DE.UTF-8";
-    LC_IDENTIFICATION = "de_DE.UTF-8";
-    LC_MEASUREMENT = "de_DE.UTF-8";
-    LC_MONETARY = "de_DE.UTF-8";
-    LC_NAME = "de_DE.UTF-8";
-    LC_NUMERIC = "de_DE.UTF-8";
-    LC_PAPER = "de_DE.UTF-8";
-    LC_TELEPHONE = "de_DE.UTF-8";
-    LC_TIME = "de_DE.UTF-8";
-  };
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.martin = {
-    isNormalUser = true;
-    description = "Martin Wiebe";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
-  };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-	git
-	vim
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+    services.openssh.enable = true;
 
     nix.settings.experimental-features = [
         "nix-command"
         "flakes"
     ];
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
+    system.stateVersion = "25.11"; # Did you read the comment?
 
 }
--- hosts/thinkpadT14/configuration.nix
+++ hosts/thinkpadT14/configuration.nix
@@ -4,6 +4,9 @@
     imports = [
         ./hardware-configuration.nix
 
+        ../../modules/system/systemd-boot.nix
+        ../../modules/system/locals.nix
+
         ../../modules/windowmanager/mango.nix
         ../../modules/windowmanager/hyprland.nix
 
@@ -18,11 +21,6 @@
         ../../modules/misc/zram.nix
     ];
 
-    # Bootloader.
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.systemd-boot.configurationLimit = 3;
-    boot.loader.efi.canTouchEfiVariables = true;
-
     hardware = {
         graphics = {
             enable = true;
@@ -35,27 +33,11 @@
         };
     };
 
-    networking.hostName = "thinkpadt14"; # Define your hostname.
-
-    time.timeZone = "Europe/Berlin";
-
-    i18n.defaultLocale = "en_US.UTF-8";
-
-    i18n.extraLocaleSettings = {
-        LC_ADDRESS = "de_DE.UTF-8";
-        LC_IDENTIFICATION = "de_DE.UTF-8";
-        LC_MEASUREMENT = "de_DE.UTF-8";
-        LC_MONETARY = "de_DE.UTF-8";
-        LC_NAME = "de_DE.UTF-8";
-        LC_NUMERIC = "de_DE.UTF-8";
-        LC_PAPER = "de_DE.UTF-8";
-        LC_TELEPHONE = "de_DE.UTF-8";
-        LC_TIME = "de_DE.UTF-8";
-    };
+    networking.hostName = "thinkpadt14";
 
     services.xserver.enable = true;
     services.xserver.xkb = {
-        layout = "us";
+        layout = "eu";
         variant = "";
     };
 
