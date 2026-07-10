b65333c1df32040d975e13937623c9f6071fc6dc
Martin Wiebe <m.wiebe311204@gmail.com>
2026-07-10 12:55:02 +0200

    nixos: switch 2026-07-10T12:55:02+02:00

--- home/applications/opencode/opencode.nix
+++ home/applications/opencode/opencode.nix
@@ -230,6 +230,10 @@
         };
         tui = {
             theme = "gruvbox-transparent";
+            keybinds = {
+                leader = "space";
+            };
+            plugin = [ "vimcode@git+https://github.com/oribarilan/vimcode.git#v0.15.3" ];
         };
     };
 }
